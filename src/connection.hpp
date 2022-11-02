18c7ad682baafa4aa5ab405a97c179abc3dce5a3
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2022-11-02 14:07:17 -0400

    Add server and connection classes

---
+++ src/connection.hpp
@@ -0,0 +1,76 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022 Dimitry Ishenko
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+//
+// Distributed under the GNU GPL license. See the LICENSE.md file for details.
+
+////////////////////////////////////////////////////////////////////////////////
+#ifndef CONNECTION_HPP
+#define CONNECTION_HPP
+
+////////////////////////////////////////////////////////////////////////////////
+#include <asio.hpp>
+#include <functional>
+#include <memory>
+#include <string>
+
+////////////////////////////////////////////////////////////////////////////////
+class connection : public std::enable_shared_from_this<connection>
+{
+    using tcp = asio::ip::tcp;
+
+public:
+    static auto create(tcp::socket sock)
+    {
+        return std::shared_ptr<connection>(new connection{std::move(sock)});
+    }
+
+    using recv_cb = std::function<void(std::string)>;
+    void on_recv(recv_cb cb) { recv_cb_ = std::move(cb); }
+
+    void start() { async_wait(); }
+
+private:
+    tcp::socket socket_;
+    recv_cb recv_cb_;
+    std::string data_;
+
+    explicit connection(tcp::socket sock) : socket_{std::move(sock)} { }
+
+    void async_wait()
+    {
+        // NB: Using shared_from_this() creates a copy of the
+        // shared pointer, which is then bound with the recv() function
+        // and passed to asio::io_context using async_await().
+        // This keeps the connection alive for as long as it is
+        // waiting for data.
+
+        socket_.async_wait(tcp::socket::wait_read,
+            std::bind(&connection::recv, shared_from_this(), std::placeholders::_1)
+        );
+    }
+
+    void recv(asio::error_code ec)
+    {
+        if(!ec)
+        {
+            std::string data(socket_.available(), '\0');
+            socket_.receive(asio::buffer(data));
+            data_ += data;
+
+            for(std::size_t p; (p = data_.find('\n')) != data_.npos; )
+            {
+                auto cmd = data_.substr(0, p);
+                if(cmd.back() == '\r') cmd.pop_back();
+                data_.erase(0, p + 1);
+
+                if(recv_cb_) recv_cb_(std::move(cmd));
+            }
+
+            async_wait();
+        }
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+#endif
---
+++ src/server.hpp
@@ -0,0 +1,71 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022 Dimitry Ishenko
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+//
+// Distributed under the GNU GPL license. See the LICENSE.md file for details.
+
+////////////////////////////////////////////////////////////////////////////////
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+////////////////////////////////////////////////////////////////////////////////
+#include <asio.hpp>
+#include <functional>
+#include <string>
+#include <string_view>
+
+////////////////////////////////////////////////////////////////////////////////
+class server
+{
+    using string = std::string;
+    using string_view = std::string_view;
+    using tcp = asio::ip::tcp;
+
+public:
+    server(asio::io_context& ctx, string_view address, string_view port) :
+        acceptor_{ctx, make_endpoint(address, port)}
+    { }
+
+    void start() { async_accept(); }
+
+    using accept_cb = std::function<void(tcp::socket)>;
+    void on_accepted(accept_cb cb) { accept_cb_ = std::move(cb); }
+
+private:
+    tcp::acceptor acceptor_;
+    accept_cb accept_cb_;
+
+    void async_accept()
+    {
+        acceptor_.async_accept([=](auto ec, tcp::socket peer)
+        {
+            if(!ec)
+            {
+                if(accept_cb_) accept_cb_(std::move(peer));
+                async_accept();
+            }
+        });
+    }
+
+    tcp::endpoint make_endpoint(string_view address, string_view port)
+    {
+        asio::error_code ec;
+        auto address_ = asio::ip::make_address(address, ec);
+
+        if(ec) throw std::invalid_argument{
+            "Invalid address '" + std::string{address} + "'"
+        };
+
+        char* end;
+        unsigned short port_ = std::strtol(port.data(), &end, 0);
+
+        if(end != port.data() + port.size()) throw std::invalid_argument{
+            "Invalid port # '" + std::string{port} + "'"
+        };
+
+        return tcp::endpoint{address_, port_};
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+#endif
